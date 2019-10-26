bf56e92d04ed2a8e4bd87177de9ff8975d5231b7
Joshua Costa <costa.joshua27@gmail.com>
2019-10-26 04:12:37 -0700

    refactored html files and routes

--- flaskdriver/main/routes.py
+++ flaskdriver/main/routes.py
@@ -5,15 +5,21 @@ from flaskdriver.forms import AddGroceryItemForm
 
 main = Blueprint("main", __name__)
 
-@main.route("/", methods=['GET', 'POST'])
-@main.route("/home", methods=['GET', 'POST'])
+@main.route("/")
+@main.route("/home")
 def home():
+    title = "Home"
+    return render_template("home.html", title=title)
+
+@main.route("/groceries", methods=['GET', 'POST'])
+def pick_groceries():
+    title = "Pick the groceries you want to get"
     form = AddGroceryItemForm()
     if form.validate_on_submit():
         new_item = GroceryItem(name=form.name.data)
         db.session.add(new_item)
         db.session.commit()
-        return redirect(url_for('main.home'))
+        return redirect(url_for('main.pick_groceries'))
 
     groceries = GroceryItem.query.order_by(GroceryItem.name).all()
-    return render_template("home.html", title="Home", groceries=groceries, form=form)
+    return render_template("pick_groceries.html", title=title, groceries=groceries, form=form)
