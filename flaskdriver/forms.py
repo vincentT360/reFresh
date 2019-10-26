0aa93bcf9a621f3f7850d07be5822839dc3357ae
Joshua Costa <costa.joshua27@gmail.com>
2019-10-26 13:40:09 -0700

    added next step in flow

--- flaskdriver/forms.py
+++ flaskdriver/forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class AddIngredientForm(FlaskForm):
     name = StringField('New Ingredient', validators=[DataRequired()])
     submit = SubmitField('Add ingredient')
+
+class ChooseRecipeForm(FlaskForm):
+    select = SelectField('Choose recipe', coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Choose recipe')
--- flaskdriver/main/routes.py
+++ flaskdriver/main/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, Blueprint, redirect, url_for
 from flaskdriver import db
-from flaskdriver.models import Ingredient
-from flaskdriver.forms import AddIngredientForm
+from flaskdriver.models import Ingredient, IngredientProduct
+from flaskdriver.forms import AddIngredientForm, ChooseRecipeForm
 from APIs.walmartRetrieval import WalmartApi
 from APIs.spoonacular_handler import Spoonacular
 import pint
@@ -29,10 +29,24 @@ def pick_ingredients():
 
 @main.route("/recipes", methods=['GET', 'POST'])
 def get_recipes():
+    form = ChooseRecipeForm()
     title = "Choose recipes"
     ingredients = [ingredient.name for ingredient in Ingredient.query.order_by(Ingredient.name).all()]
     reg = pint.UnitRegistry()
     spoonacular = Spoonacular(reg)
     recipes = spoonacular.find_by_ingredients(ingredients)
+    form.select.choices = [(recipe.sp_id, recipe.title) for recipe in recipes]
+
+    if form.validate_on_submit():
+        chosen_recipe = {recipe.sp_id : recipe for recipe in recipes}[form.select.data]
+        for v in chosen_recipe.ingredients.values():
+            new_ingredient = IngredientProduct(name=v.name, image_url=v.image, price=0, quantity=v.amount, quantity_type=str(v.unit))
+            db.session.add(new_ingredient)
+            db.session.commit()
+        return redirect(url_for('main.get_products'))
     
-    return render_template("recipes.html", title=title, recipes=recipes)
+    return render_template("recipes.html", title=title, recipes=recipes, form=form)
+
+@main.route("/products")
+def get_products():
+    return render_template("get_products.html")
