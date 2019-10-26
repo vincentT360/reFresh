108c01161a02ed257210b8731e351b98f9dc783b
Joshua Costa <costa.joshua27@gmail.com>
2019-10-26 15:05:24 -0700

    added search functionality

--- flaskdriver/main/routes.py
+++ flaskdriver/main/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, Blueprint, redirect, url_for
 from flaskdriver import db
 from flaskdriver.models import Ingredient, IngredientProduct
-from flaskdriver.forms import AddIngredientForm, ChooseRecipeForm
+from flaskdriver.forms import AddIngredientForm, ChooseRecipeForm, SearchRecipeForm
 from APIs.walmartRetrieval import WalmartApi
 from APIs.spoonacular_handler import Spoonacular
 import pint
@@ -14,7 +14,7 @@ def home():
     title = "Home"
     return render_template("home.html", title=title)
 
-@main.route("/ingredients", methods=['GET', 'POST'])
+@main.route("/pick-ingredients", methods=['GET', 'POST'])
 def pick_ingredients():
     title = "Pick the groceries you want to get"
     form = AddIngredientForm()
@@ -27,8 +27,36 @@ def pick_ingredients():
     ingredients = Ingredient.query.order_by(Ingredient.name).all()
     return render_template("pick_ingredients.html", title=title, ingredients=ingredients, form=form)
 
-@main.route("/recipes", methods=['GET', 'POST'])
-def get_recipes():
+@main.route("/search-recipes")
+def search_for_recipes():
+    title = "Search for a recipe"
+    form = SearchRecipeForm()
+    if form.validate_on_submit():
+        reg = pint.UnitRegistry()
+        spoonacular = Spoonacular(reg)
+        recipes = None
+        return redirect(url_for('main.get_recipes', recipes=recipes))
+
+    
+    return render_template("search_for_recipes.html", title=title, form=form)
+
+@main.route("/recipes-from-search", methods=['GET', 'POST'])
+def get_recipes_from_search(recipes):
+    form = ChooseRecipeForm()
+    title = "Choose recipes"
+    form.select.choices = [(recipe.sp_id, recipe.title) for recipe in recipes]
+
+    if form.validate_on_submit():
+        chosen_recipe = {recipe.sp_id : recipe for recipe in recipes}[form.select.data]
+        for v in chosen_recipe.ingredients.values():
+            new_ingredient = IngredientProduct(name=v.name, image_url=v.image, price=0, quantity=v.amount, quantity_type=str(v.unit))
+            db.session.add(new_ingredient)
+            db.session.commit()
+        redirect(url_for('main.get_products'))
+    return render_template("recipes.html", title=title, recipes=recipes, form=form) #finish this
+
+@main.route("/recipes-from-ingredients", methods=['GET', 'POST'])
+def get_recipes_from_ingredients():
     form = ChooseRecipeForm()
     title = "Choose recipes"
     ingredients = [ingredient.name for ingredient in Ingredient.query.order_by(Ingredient.name).all()]
