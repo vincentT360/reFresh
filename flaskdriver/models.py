f19094e8da15e92f0896da151957a33f6ce61139
Joshua Costa <costa.joshua27@gmail.com>
2019-10-26 19:50:46 -0700

    changed the db

--- flaskdriver/models.py
+++ flaskdriver/models.py
@@ -11,3 +11,14 @@ class IngredientProduct(db.Model):
     price = db.Column(db.Float)
     quantity = db.Column(db.Float)
     quantity_type = db.Column(db.String(120))
+    meal_id = db.Column(db.Integer, db.ForeignKey('meal.id'), nullable=False)
+
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary=True)
+    ingredients = db.relationship('IngredientProduct', backref='recipe', lazy=True)
+    mealplan_id = db.Column(db.Integer, db.ForeignKey('mealplan.id'), nullable=False)
+
+class MealPlan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    meals = db.relationship('Meal', backref='meal_plan', lazy=True)
+
